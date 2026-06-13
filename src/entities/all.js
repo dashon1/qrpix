3e0ff932dc641114481ddbf36d4fbe3106f9d796
dashon1 <dashon1@github.com>
2026-06-13 17:56:55 -0400

    Add missing entity stubs (Lead, Article, all)

---
+++ src/entities/all.js
@@ -0,0 +1,4 @@
+import { base44 } from '../api/base44Client';
+export const Photo = base44.entities.Photo;
+export const Reaction = base44.entities.Reaction;
+export const Comment = base44.entities.Comment;
